b645286c68944ed7917fcce15c88251b80e05203
lector2140 <45631218+lector2140@users.noreply.github.com>
2019-01-31 14:00:16 +0200

    Delete Shape.java

--- src/main/java/p77/Shape.java
+++
@@ -1,8 +0,0 @@
-package p77;
-
-public abstract class Shape {
-    abstract double calculatePerimetr();
-    abstract double calculateArea();
-    public String getName() {
-        return this.getClass().getSimpleName();}
-}
